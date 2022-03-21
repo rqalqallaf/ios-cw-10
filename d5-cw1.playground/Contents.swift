13ee4956b66b1cfa4294aa8da724bc54cad05a4a
rqalqallaf <rq.alqallaf@gmail.com>
2022-03-21 21:21:13 +0300

    tm

--- d5-cw1.playground/Contents.swift
+++ d5-cw1.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let family = ["abdulrazaq", "barrak", "alqallaf"]
+print(family.count)
+
+var evenNumbers = [2,4,6,8,10,12]
+evenNumbers.remove(at: 2)
+
+print(evenNumbers)
+
+evenNumbers += [16,18,20,22]
+
+var money = [1.00, 2.00, 3.00]
+money.randomElement()
+
+money.removeAll()
+
+var numbers = [1,2,3,4,5]
+
+for i in numbers{
+    print(i)
+}
+
+numbers += [6,7,8,9,10]
+
+for i in numbers{
+    print(i)
+}
